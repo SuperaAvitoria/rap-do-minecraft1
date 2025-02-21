3e071cc4990f44d310c65e05877da555b50a31a6
SuperaAvitoria <matheusvasquesprado@gmail.com>
2025-02-21 09:23:58 -0300

    Add files via upload

---
+++ godofredo/exemplo/exemlpo.js
@@ -0,0 +1,26 @@
+algoritmo "soma"
+
+var
+
+  n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13,n14,n15,n16,n17,n18,n19,n20, r1, r2, r3, r4 : real
+
+  inicio
+                                                            
+     Escreva ("Digite o primeiro número:")
+     Leia (n1)
+     Escreva ("Digite o segundo número:")
+     Leia (n2)
+     
+     r1 := n1+n2+n3+n4+n5+n6+n7+n8+n9+n10+n11+n12+n13+n14+n15+n16+n17+n18+n19+n20
+     r2 := n1-n2-n3-n4-n5-n6-n7n8n9n10n11n12n13n14n15n16n17n18n19n20
+     r3 := n1*n2
+     r4 := n1/n2
+
+     
+     Escreval ("Soma:", r1)
+     Escreval ("Subtração:", r2)
+     Escreval ("Multiplicação:", r3)
+     Escreval ("Divisão:", r4)
+    
+
+     
