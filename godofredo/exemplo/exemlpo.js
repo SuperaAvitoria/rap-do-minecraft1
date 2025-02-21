5030b0eb5d1954353537434731c88593f8645916
SuperaAvitoria <matheusvasquesprado@gmail.com>
2025-02-21 10:22:46 -0300

    Update exemlpo.js

--- godofredo/exemplo/exemlpo.js
+++ godofredo/exemplo/exemlpo.js
@@ -3,17 +3,52 @@ algoritmo "soma"
 var
 
   n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13,n14,n15,n16,n17,n18,n19,n20, r1, r2, r3, r4 : real
-
   inicio
                                                             
      Escreva ("Digite o primeiro número:")
      Leia (n1)
      Escreva ("Digite o segundo número:")
      Leia (n2)
-     
+     Escreva ("Digite o terceiro número:")
+     Leia (n3)
+     Escreva ("Digite o quarto número:")
+     Leia (n4)
+     Escreva ("Digite o quinto número:")
+     Leia (n5)
+     Escreva ("Digite o sexto número:")
+     Leia (n6)
+     Escreva ("Digite o setimo número:")
+     Leia (n7)
+     Escreva ("Digite o oitavo número:")
+     Leia (n8)
+     Escreva ("Digite o nono número:")
+     Leia (n9)
+     Escreva ("Digite o  decimo número:")
+     Leia (n10)
+     Escreva ("Digite o decimo primeiro número:")
+     Leia (n11)
+     Escreva ("Digite o  decimo segundo número:")
+     Leia (n12) 
+     Escreva ("Digite o decimo terceiro número:")
+     Leia (n13)
+     Escreva ("Digite o  decimo quarto número:")
+     Leia (n14)  
+     Escreva ("Digite o decimo quinto número:")
+     Leia (n15)
+     Escreva ("Digite o  decimo sexto número:")
+     Leia (n16)  
+     Escreva ("Digite o decimo setimo número:")
+     Leia (n17)
+     Escreva ("Digite o  decimo oitavo número:")
+     Leia (n18)
+     Escreva ("Digite o decimo nono número:")
+     Leia (n19)
+     Escreva ("Digite o  vintesimo número:")
+     Leia (n20)
+
      r1 := n1+n2+n3+n4+n5+n6+n7+n8+n9+n10+n11+n12+n13+n14+n15+n16+n17+n18+n19+n20
-     r2 := n1-n2-n3-n4-n5-n6-n7n8n9n10n11n12n13n14n15n16n17n18n19n20
-     r3 := n1*n2
+     r2 := n1-n2-n3-n4-n5-n6-n7-n8-n9-n10-n11-n12-n13-n14-n15-n16-n17-n18-n19-n20
+     r3 := n1*n2*n3n4n5n6n7n8n9n10n11n12n13n14n15n16n17n18n19n20
      r4 := n1/n2
 
      
